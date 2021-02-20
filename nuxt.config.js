284ff45e576db1f599d3105e24e1cea1e0f71276
Risha Sringa Changmai <michael.the.don@gmail.com>
2021-02-20 11:52:47 +0530

    fixed hotel transfer maps

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,7 +99,7 @@ export default {
     authenticationType: 'Bearer',
     clientConfigs: {
       default: {
-        httpEndpoint: 'http://api.skyvx.com/graphql'
+        httpEndpoint: 'https://api.skyvx.com/graphql'
       }
     },
     defaultOptions: {
